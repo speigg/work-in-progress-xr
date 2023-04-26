afad75ef3d43644fd113190d5cce14aa9f80169b
Gheric Speiginer <gheric.speiginer@gmail.com>
2023-04-26 07:54:11 -0700

    Add demo scene

---
+++ src/systems/GrabbableScreenshareSystem.ts
@@ -0,0 +1,10 @@
+import { defineSystem } from "@etherealengine/engine/src/ecs/functions/SystemFunctions"
+
+const execute = () => {
+
+}
+
+export default defineSystem({
+  uuid: 'ee.productivity.grabbable-screenshare',
+  execute
+})
