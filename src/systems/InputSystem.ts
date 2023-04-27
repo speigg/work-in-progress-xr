bef4dc249b457ac809b86fa47587c3d68224fab3
Gheric Speiginer <gheric.speiginer@gmail.com>
2023-04-27 13:54:09 -0700

    Update InputSystem.ts

--- src/systems/InputSystem.ts
+++ src/systems/InputSystem.ts
@@ -4,10 +4,13 @@ import { InputComponent } from "@etherealengine/engine/src/input/components/Inpu
 import { InputSourceComponent } from "@etherealengine/engine/src/input/components/InputSourceComponent"
 import { GrabbableComponent } from "../GrabbableComponent"
 import { NameComponent } from "@etherealengine/engine/src/scene/components/NameComponent"
-import { LocalTransformComponent } from "@etherealengine/engine/src/transform/components/TransformComponent"
+import { LocalTransformComponent, TransformComponent } from "@etherealengine/engine/src/transform/components/TransformComponent"
 import { addObjectToGroup } from "@etherealengine/engine/src/scene/components/GroupComponent"
 import { Color, Mesh, MeshBasicMaterial, SphereGeometry } from "three"
 import { VisibleComponent } from "@etherealengine/engine/src/scene/components/VisibleComponent"
+import { BoundingBoxDynamicTag } from "@etherealengine/engine/src/interaction/components/BoundingBoxComponents"
+import { ComputedTransformComponent } from "@etherealengine/engine/src/transform/components/ComputedTransformComponent"
+import { Entity } from "@etherealengine/engine/src/ecs/classes/Entity"
 
 const namedObjects = defineQuery([NameComponent])
 const grabbables = defineQuery([GrabbableComponent, InputComponent])
@@ -29,12 +32,20 @@ const InteractionIndicatorComponent = defineComponent({
   }
 })
 
+const followInputSource = (grabbableEid:Entity, sourceEid: Entity) => {
+  const grabbableTransform = getComponent(grabbableEid, TransformComponent)
+  const sourceTransform = getComponent(sourceEid, TransformComponent)
+  grabbableTransform.position.copy(sourceTransform.position)
+  grabbableTransform.rotation.copy(sourceTransform.rotation)
+}
+
 const execute = () => {
 
   for (const eid of namedObjects.enter()) {
     const name = getComponent(eid, NameComponent)
     if (name.endsWith('Grabbable')) {
       setComponent(eid, GrabbableComponent)
+      setComponent(eid, BoundingBoxDynamicTag)
     }
   }
 
@@ -42,9 +53,9 @@ const execute = () => {
 
   for (const eid of inputSources()) {
     const inputSource = getComponent(eid, InputSourceComponent)
-    const isActive = inputSource.assignedEntity
+    const assignedEntity = inputSource.assignedEntity
     const indicator = getComponent(eid, InteractionIndicatorComponent)
-    const targetColor = color.set(isActive ? 'lightgreen' : 'lightgray')
+    const targetColor = color.set(assignedEntity ? 'lightgreen' : 'lightgray')
     indicator.sphereMaterial.color.lerp(targetColor, 0.1)
   }
 
@@ -52,12 +63,16 @@ const execute = () => {
     const input = getComponent(eid, InputComponent)    
     for (const sourceEid of input.inputSources) {
       const inputSource = getComponent(sourceEid, InputSourceComponent)
-      if (inputSource.buttons.Trigger?.down) {
-        setComponent(eid, LocalTransformComponent, {parentEntity: sourceEid})
+      if (inputSource.source.gamepad?.buttons[0].pressed || inputSource.source.gamepad?.buttons[1].pressed) {
+        setComponent(eid, ComputedTransformComponent, {referenceEntity: sourceEid, computeFunction:followInputSource })
+        const indicator = getComponent(sourceEid, InteractionIndicatorComponent)
+        const targetColor = color.set('red')
+        indicator.sphereMaterial.color.set(targetColor)
       } else {
-        removeComponent(eid, LocalTransformComponent)
+        removeComponent(eid, ComputedTransformComponent)
       }
     }
+    if (input.inputSources.length === 0) removeComponent(eid, ComputedTransformComponent)
   }
 }
 
